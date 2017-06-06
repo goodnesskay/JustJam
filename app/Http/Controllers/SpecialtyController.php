c6806b2f957637354ed4f39bbbcf3204bdf37e22
goodnesskay <gtkbrain@gmail.com>
2017-06-07 00:23:59 +0100

    Writing models, migrations and functions in controllers

--- app/Http/Controllers/SpecialtyController.php
+++ app/Http/Controllers/SpecialtyController.php
@@ -3,19 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Specialty;
+use Illuminate\Support\Facades\DB;
 
 class SpecialtyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-
-    }
-
     /**
      * Show the form for creating a new resource.
      *
@@ -23,7 +16,7 @@ class SpecialtyController extends Controller
      */
     public function create()
     {
-        //
+        return view('dashboaard.new-specialty',['users'=>Auth::User()]);
     }
 
     /**
@@ -34,7 +27,24 @@ class SpecialtyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request,[
+            'specialty_name'=>'required',
+            'years_of_experience'=>'required|integer',
+            'brief_description'=>'required|min:100'
+        ]);
+
+        $specialties =  new Specialty();
+        $specialties->specialty_name = $request->input('specialty_name');
+        $specialties->years_of_experience = $request->input('years_of_experience');
+        $specialties->brief_description = $request->input('brief_description');
+        $specialties->created_by = Auth::User()->id;
+
+        if ($specialties->save())
+        {
+            return redirect()->back()->with('alert','Kudos! You have added '.$specialties->specialty_name.' to your specialties');
+        }else{
+            return redirect()->back()->withErrors($validator);
+        }
     }
 
     /**
@@ -43,9 +53,10 @@ class SpecialtyController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show()
     {
-        //
+        $specialties = DB::table('specialties')->select('*')->where('created_by','==',Auth::User()->id)->paginate(10);
+        return view('dashboard.my-specialties',['users', Auth::User()])->with('specialties',$specialties);
     }
 
     /**
@@ -56,7 +67,8 @@ class SpecialtyController extends Controller
      */
     public function edit($id)
     {
-        //
+        $specialties = Specialty::find($id);
+        return view('dashboard.edit-my-specialties',['users', Auth::User()])->with('specialties',$specialties);
     }
 
     /**
@@ -68,7 +80,23 @@ class SpecialtyController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request,[
+            'specialty_name'=>'required',
+            'years_of_experience'=>'required|integer',
+            'brief_description'=>'required|min:100'
+        ]);
+
+        $specialties =  Specialty::find($id);
+        $specialties->specialty_name = $request->input('specialty_name');
+        $specialties->years_of_experience = $request->input('years_of_experience');
+        $specialties->brief_description = $request->input('brief_description');
+
+        if ($specialties->save())
+        {
+            return redirect()->back()->with('alert','Kudos! You have updated '.$specialties->specialty_name.' in your list of specialties');
+        }else{
+            return redirect()->back()->withErrors($validator);
+        }
     }
 
     /**
@@ -79,6 +107,13 @@ class SpecialtyController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $specialties = Specialty::find($id);
+        if ($specialties->destroy())
+        {
+            return redirect()->back()->with('alert','Hello! You just deleted '.$specialties->specialty_name.' from your list of specialties');
+        }else{
+            return redirect()->back()->with('alert','An error occurred due to poor network');
+        }
+
     }
 }
