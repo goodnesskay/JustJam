5af291799bdf3f858bdb85831723556b43bade61
goodnesskay <gtkbrain@gmail.com>
2017-06-07 02:17:22 +0100

    Made changes to config file

---
+++ database/migrations/2017_06_06_234800_create_performances_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerformancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('');
+            $table->string('');
+            $table->string('');
+            $table->string('');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('performances');
+    }
+}
